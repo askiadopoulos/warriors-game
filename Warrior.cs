76fd0deeb91a682471670a4073619822645b3cf3
Angelos Skiadopoulos <std095292@ac.eap.gr>
2019-10-23 15:18:32 +0300

    Add class Warrior to project

---
+++ Warrior.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarriorsGame
+{
+    class Warrior
+    {
+        // Properties initiated with default values
+        public string Name { get; set; } = "Warrior";
+        public double Health { get; set; } = 0;
+        public double AttackMax { get; set; } = 0;
+        public double BlockMax { get; set; } = 0;
+
+        // TODO
+
+        // Random numbers
+        private readonly Random rnd = new Random();
+
+        // Parameter Constructor
+        public Warrior(string name = "Warrior", double health = 0, double attackMax = 0, double blockMax = 0)
+        {
+            Name = name;
+            Health = health;
+            AttackMax = attackMax;
+            BlockMax = blockMax;
+        }
+
+        // Attack
+        // Generate random attack from one to the maximum attack value
+        public double Attack()
+        {
+            return rnd.Next(1, (int)AttackMax);
+        }
+
+        // Block
+        // Generate random block from one to the maximum block value
+        public double Block()
+        {
+            return rnd.Next(1, (int)BlockMax);
+        }
+    }
+}
